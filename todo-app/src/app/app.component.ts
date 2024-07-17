ba290008abea515b16087427d08880087485baef
M.BENBAZZA_adm <benbazza.mohammed@gmail.com>
2024-07-17 15:57:30 +0200

    init commit

---
+++ todo-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER, TuiNotificationComponent, TuiNotificationModule } from "@taiga-ui/core";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {TuiInputModule} from '@taiga-ui/kit';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiNotificationModule, TuiInputModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+    providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}]
+})
+export class AppComponent {
+  title = 'todo-app';
+}
